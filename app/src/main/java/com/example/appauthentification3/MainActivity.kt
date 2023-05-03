497b0f313fceab7397a045a853255471cbc43716
robertsergo <111302645+robertsergo@users.noreply.github.com>
2023-05-02 23:55:34 -0400

    #9 formation developpeur android: interagir avec les Views, Toast, Visibility, OnClickListener, message d'erreur coloré en rouge

--- app/src/main/java/com/example/appauthentification3/MainActivity.kt
+++ app/src/main/java/com/example/appauthentification3/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.appauthentification3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -15,19 +17,26 @@ class MainActivity : AppCompatActivity() {
         val connecte = findViewById<Button>(R.id.connecter)
         val courriel = findViewById<EditText>(R.id.courriel)
         val motDePasse = findViewById<EditText>(R.id.motdepasse)
+        val er = findViewById<TextView>(R.id.erreur)
+
 
         val motDePasseValide = "12345"
         val courrielValide = "robert@gmail.com"
 
 
         connecte.setOnClickListener{
+            er.visibility = View.GONE
            // Toast.makeText(this, "vous avez cliqué sur conneter", Toast.LENGTH_LONG).show()
             val courrielEntree= courriel.text.toString()
             val motDePasseEntree = motDePasse.text.toString()
             if(courrielEntree.trim().isEmpty() || motDePasseEntree.trim().isEmpty()){
-                Toast.makeText(this, "les champs doivent etre remplis convenablement", Toast.LENGTH_LONG).show()
+                er.text = "les champs doivent etre remplis convenablement"
+                er.visibility = View.VISIBLE
+                //Toast.makeText(this, "les champs doivent etre remplis convenablement", Toast.LENGTH_LONG).show()
             }else if(courrielEntree.trim() != courrielValide || motDePasseEntree.trim() != motDePasseValide){
-            Toast.makeText(this, "user name ou mot de passe incorrect!!!", Toast.LENGTH_LONG).show()
+                er.text = "les champs doivent etre remplis convenablement"
+                er.visibility = View.VISIBLE
+           // Toast.makeText(this, "user name ou mot de passe incorrect!!!", Toast.LENGTH_LONG).show()
         }else if(courrielEntree.trim() == courrielValide && motDePasseEntree.trim() == motDePasseValide){
                 Toast.makeText(this, "Bienvenue $courrielEntree", Toast.LENGTH_LONG).show()
             }
