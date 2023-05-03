04fd3246eced1c9c16c322995b91b4c7335c65c6
robertsergo <111302645+robertsergo@users.noreply.github.com>
2023-05-02 22:34:52 -0400

    authen avec relativeLayout mot de passe et courriel

--- app/src/main/java/com/example/appauthentification3/MainActivity.kt
+++ app/src/main/java/com/example/appauthentification3/MainActivity.kt
@@ -3,15 +3,37 @@ package com.example.appauthentification3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         val connecte = findViewById<Button>(R.id.connecter)
+        val courriel = findViewById<EditText>(R.id.courriel)
+        val motDePasse = findViewById<EditText>(R.id.motdepasse)
+
+        val motDePasseValide = "12345"
+        val courrielValide = "robert@gmail.com"
+
+
         connecte.setOnClickListener{
-            Toast.makeText(this, "vous avez cliqué sur conneter", Toast.LENGTH_LONG)
+           // Toast.makeText(this, "vous avez cliqué sur conneter", Toast.LENGTH_LONG).show()
+            val courrielEntree= courriel.text.toString()
+            val motDePasseEntree = motDePasse.text.toString()
+            if(courrielEntree.trim().isEmpty() || motDePasseEntree.trim().isEmpty()){
+                Toast.makeText(this, "les champs doivent etre remplis convenablement", Toast.LENGTH_LONG).show()
+            }else if(courrielEntree.trim() != courrielValide || motDePasseEntree.trim() != motDePasseValide){
+            Toast.makeText(this, "user name ou mot de passe incorrect!!!", Toast.LENGTH_LONG).show()
+        }else if(courrielEntree.trim() == courrielValide && motDePasseEntree.trim() == motDePasseValide){
+                Toast.makeText(this, "Bienvenue $courrielEntree", Toast.LENGTH_LONG).show()
+            }
+
+
+
         }
     }
 }
