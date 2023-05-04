6b9d8305a64a73c921324d3d2366b4767138961a
robertsergo <111302645+robertsergo@users.noreply.github.com>
2023-05-03 22:57:44 -0400

    Ajout de ListeView non personalisée 2

--- app/src/main/java/com/example/appauthentification3/HomeActivity.kt
+++ app/src/main/java/com/example/appauthentification3/HomeActivity.kt
@@ -2,6 +2,8 @@ package com.example.appauthentification3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
@@ -10,7 +12,11 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         //1-recuper la variable
-        var courriel = intent.getStringExtra("email")
+        val courriel = intent.getStringExtra("email")
+        val listePost = findViewById<ListView>(R.id.listepost)
+        val postsArray = listOf("post 6, post 7, post 8, post 9, post 10, post 11")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, postsArray)
+        listePost.adapter = adapter
 
 
 
